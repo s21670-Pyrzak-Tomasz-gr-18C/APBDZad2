7044b81507acc2a71393ba864600418ef36cba01
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2022-06-06 23:23:23 +0200

    resolved

---
+++ XYZExporter/XYZExporter/Program.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XYZExporter
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using StreamWriter sw = new StreamWriter("../../../Files/log.txt");
+            if (args.Length != 3)
+            {
+                throw new Exception("wrong number of arguments");
+            }
+
+            string pathToDataFile = "";
+            string pathToOutputFile = "";
+
+            if (!File.Exists(Path.GetFullPath(args[0])))
+            {
+                sw.WriteLine($"File not found: {args[0]}");
+            }
+            else
+            {
+                pathToDataFile = Path.GetFullPath(args[0]);
+                Console.WriteLine(pathToDataFile);
+            }
+
+            try
+            {
+                using (File.Create(args[1] + ".jason")) ;
+                pathToOutputFile = Path.GetFullPath(args[1] + ".jason");
+                Console.WriteLine(pathToOutputFile);
+            }
+            catch (Exception e)
+            {
+                sw.WriteLine(e.Message);
+            }
+
+            if (args[2] == "jason")
+            {
+                string[] result2 = await File.ReadAllLinesAsync(pathToDataFile);
+                bool validated = true;
+
+                var studenci = new JArray();
+
+                foreach (string line in result2)
+                {
+                    string alineData = line;
+
+                    string[] studentData = alineData.Split(',');
+
+                    if ((studentData.Length != 9))
+                    {
+                        validated = false;
+                        sw.WriteLine($"Not described by 9 data columns: {alineData}");
+                    }
+                    else
+                    {
+                        foreach (string data in studentData)
+                        {
+                            if (data == "")
+                            {
+                                validated = false;
+                                sw.WriteLine($"One or more columns are empty: {alineData}");
+                            }
+                        }
+                    }
+
+                    if (validated)
+                    {
+                        var newStudent = new JObject(
+                            new JProperty("indexNumber", "s" + studentData[4]),
+                            new JProperty("fname", studentData[0]),
+                            new JProperty("lname", studentData[1]),
+                            new JProperty("birthdate", studentData[5]),
+                            new JProperty("email", studentData[6]),
+                            new JProperty("mothersName", studentData[7]),
+                            new JProperty("fathersName", studentData[8]),
+                            new JProperty("studiesName", studentData[2]),
+                            new JProperty("studiesMode", studentData[3])
+                            );
+
+                        if (studenci.Count == 0)
+                        {
+                            studenci.Add(newStudent);
+                        }
+
+                        if (studenci.Contains(newStudent))
+                        {
+                            sw.WriteLine("Student exist" + newStudent.ToString());
+                        }
+                        else
+                        {
+                            studenci.Add(newStudent);
+                        }
+                    }
+                    var jObject = new JObject(
+                    new JProperty("uczelnia", new JObject(
+                        new JProperty("createdAt", DateTime.Today.ToString("dd.MM.yyyy")),
+                        new JProperty("author", "Tomasz Pyrzak"),
+                        new JProperty("studenci", studenci)
+                    ))
+                    );
+                    File.WriteAllText(pathToOutputFile, jObject.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ XYZExporter/XYZExporter/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace XYZExporter
+{
+    internal class Student
+    {
+        public Student(string v1, string v2, string v3, string v4, string v5, string v6, string v7, string v8, string v9)
+        {
+        }
+
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string studiesName { get; set; }
+        public string studiesMode { get; set; }
+        public int indexNumber { get; set; }
+        public DateTime birthdate { get; set; }
+        public string email { get; set; }
+        public string fathersName { get; set; }
+        public string mothersName { get; set; }
+    }
+
+
+}
